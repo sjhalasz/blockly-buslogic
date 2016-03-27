1948bec8626b995c6b4bae83948e7b7ae3e8228c
Steven J. Halasz <sjhalasz@users.noreply.github.com>
2016-03-27 19:08:01 -0400

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,39 @@
+Blockly.Blocks['settlement_days'] = {
+  init: function() {
+    this.appendStatementInput("days")
+        .setCheck(null)
+        .appendField("Settlement Days");
+    this.setInputsInline(false);
+    this.setColour(120);
+    this.setTooltip('This routine calculates settlement days.');
+  }
+};
+
+Blockly.Blocks['set_variable'] = {
+  init: function() {
+    this.appendStatementInput("NAME")
+        .setCheck(null)
+        .appendField("set")
+        .appendField(new Blockly.FieldVariable("<variable name>"), "variable")
+        .appendField("to");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['client'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("client")
+        .appendField(new Blockly.FieldDropdown([["AFV", "afv"], ["BFF", "bff"], ["DNC", "dnc"], ["GBF", "gbf"], ["JQF", "jqf"], ["JSF", "jsf"], ["LLB", "llb"], ["MSB", "msb"], ["PPR", "ppr"]]), "NAME");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(20);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
